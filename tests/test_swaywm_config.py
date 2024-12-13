4e11110deb26c1e668a3bed5ba14b0594c5a97b6
Michal Kielan <michalkielan@protonmail.com>
2024-12-13 08:27:44 +0100

    Add python test

---
+++ tests/test_swaywm_config.py
@@ -0,0 +1,13 @@
+""" swaywm config tests"""
+
+import ansible_runner
+
+
+def test_minimal_examle():
+    """Minimal working example"""
+    result = ansible_runner.run(
+        private_data_dir=".",
+        inventory=["localhost"],
+        playbook="minimal_example.yml",
+    )
+    assert result.rc == 0, f"Playbook failed with {result.rc}."
