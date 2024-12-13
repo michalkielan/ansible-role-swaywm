f637de08dbe88cefab7476ec85c31241e2ac2cd1
Michal Kielan <michalkielan@protonmail.com>
2024-12-13 21:41:18 +0100

    Validate config file

--- tests/test_swaywm_config.py
+++ tests/test_swaywm_config.py
@@ -1,6 +1,27 @@
 """ swaywm config tests"""
 
+import subprocess
+from pathlib import Path
 import ansible_runner
+
+
+class SwayConfigValidationError(Exception):
+    """Exception raised when sway config is invalid."""
+
+
+def validate_sway_config(config_file):
+    """Validate sway config file"""
+    try:
+        subprocess.run(
+            ["sway", "--config", config_file, "--validate", "--debug"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+    except subprocess.CalledProcessError as exc:
+        raise SwayConfigValidationError(
+            f"Config file {config_file} is invalid: {exc.stderr.decode()}"
+        ) from exc
 
 
 def test_minimal_examle():
@@ -11,3 +32,5 @@ def test_minimal_examle():
         playbook="minimal_example.yml",
     )
     assert result.rc == 0, f"Playbook failed with {result.rc}."
+    config_file = Path.home() / ".config" / "sway" / "config"
+    validate_sway_config(config_file)
