6141c05d0e1f1af95e7b52654c6e58a29c22d19c
Michal Kielan <michalkielan@protonmail.com>
2024-12-14 21:25:01 +0100

    Parametrized tests

--- tests/test_swaywm_config.py
+++ tests/test_swaywm_config.py
@@ -1,5 +1,6 @@
 """ swaywm config tests"""
 
+import pytest
 import subprocess
 from pathlib import Path
 import ansible_runner
@@ -24,12 +25,13 @@ def validate_sway_config(config_file):
         ) from exc
 
 
-def test_minimal_examle():
+@pytest.mark.parametrize("playbook_file", ["minimal_example.yml"])
+def test_minimal_examle(playbook_file):
     """Minimal working example"""
     result = ansible_runner.run(
         private_data_dir="./tests",
         inventory=["localhost"],
-        playbook="minimal_example.yml",
+        playbook=playbook_file,
     )
     assert result.rc == 0, f"Playbook failed with {result.rc}."
     config_file = Path.home() / ".config" / "sway" / "config"
